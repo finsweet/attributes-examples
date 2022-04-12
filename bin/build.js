02f61f78c5a8c9b997931ec6c63972656403cc0b
Alex Iglesias <alexiglesias@finsweet.com>
2022-04-12 18:37:53 +0200

    Removed demo files

--- bin/build.js
+++ bin/build.js
@@ -5,7 +5,7 @@ const buildDirectory = 'dist';
 const production = process.env.NODE_ENV === 'production';
 
 // Config entrypoint files
-const entryPoints = ['src/index.ts'];
+const entryPoints = ['src/cms/populate-external-data/index.ts'];
 
 /**
  * Default Settings
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { helloStarter } from '$utils/log';
-
-// Starter example. Check the comments!
-document.addEventListener('DOMContentLoaded', () => {
-  const name = 'John Doe';
-  helloStarter(name);
-});
